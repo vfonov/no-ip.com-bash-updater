b98ef7be458f5c993baaa58e5affd69f5b18504c
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2017-10-12 10:30:57 -0400

    Changed from wget to curl

--- noipupdater.sh
+++ noipupdater.sh
@@ -1,30 +1,38 @@
 #!/bin/bash
 
 # No-IP uses emails as usernames, so make sure that you encode the @ as %40
-USERNAME=username
-PASSWORD=password
-HOST=hostsite
-LOGFILE=logdir/noip.log
-STOREDIPFILE=configdir/current_ip
-USERAGENT="Simple Bash No-IP Updater/0.4 antoniocs@gmail.com"
+USERNAME=
+PASSWORD=
+HOST=
 
-if [ ! -e $STOREDIPFILE ]; then
-	touch $STOREDIPFILE
+# TODO: set appropriate location 
+LOGFILE=/tmp/noip.log
+STOREDIPFILE=/tmp/noip.ip
+
+USERAGENT="Simple Bash No-IP Updater/0.4.1 vladimir.fonov@gmail.com"
+
+if [ x"$USERNAME"x == xx ];then
+    echo "$0: Update configuration!" 1>&2
+    exit 1
 fi
 
-NEWIP=$(wget -O - http://icanhazip.com/ -o /dev/null)
+
+if [ ! -e $STOREDIPFILE ]; then
+    touch $STOREDIPFILE
+fi
+
+NEWIP=$(curl --silent http://icanhazip.com/ )
 STOREDIP=$(cat $STOREDIPFILE)
 
 if [ "$NEWIP" != "$STOREDIP" ]; then
-	RESULT=$(wget -O "$LOGFILE" -q --user-agent="$USERAGENT" --no-check-certificate "https://$USERNAME:$PASSWORD@dynupdate.no-ip.com/nic/update?hostname=$HOST&myip=$NEWIP")
+	RESULT=$(curl  --silent --user-agent "$USERAGENT" "https://$USERNAME:$PASSWORD@dynupdate.no-ip.com/nic/update?hostname=$HOST&myip=$NEWIP")
 
 	LOGLINE="[$(date +"%Y-%m-%d %H:%M:%S")] $RESULT"
 	echo $NEWIP > $STOREDIPFILE
-else
-	LOGLINE="[$(date +"%Y-%m-%d %H:%M:%S")] No IP change"
+#else # VF: no need to tell me ech time
+#	LOGLINE="[$(date +"%Y-%m-%d %H:%M:%S")] No IP change"
+    echo $LOGLINE >> $LOGFILE
 fi
-
-echo $LOGLINE >> $LOGFILE
 
 exit 0
 
